623ea10fc6c17bdddac6aa96b04150c221245782
urijan44 <teamsva360@gmail.com>
2022-09-02 23:20:05 +0900

    Complete SequenceQueue

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SequenceQueue",
-            dependencies: []),
-        .testTarget(
-            name: "SequenceQueueTests",
-            dependencies: ["SequenceQueue"]),
+            dependencies: [])
     ]
 )
--- Sources/SequenceQueue/SequenceQueue.swift
+++ Sources/SequenceQueue/SequenceQueue.swift
@@ -1,6 +1,68 @@
-public struct SequenceQueue {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public protocol SequenceItem {
+  var timeInterval: TimeInterval { get }
+  var workItem: () -> Void { get }
+}
+
+public protocol TimeSetable {
+  func setTime(time: TimeInterval)
+}
+
+public final class SequenceTask: SequenceItem, TimeSetable {
+  public var timeInterval: TimeInterval
+  public let workItem: () -> Void
+
+  public init(timeInterval: TimeInterval, workItem: @escaping () -> Void) {
+    self.timeInterval = timeInterval
+    self.workItem = workItem
+  }
+
+  public func setTime(time: TimeInterval) {
+    self.timeInterval += time
+  }
+}
+
+public final class SequenceQueue<T> where T: TimeSetable, T: SequenceItem {
+
+  @resultBuilder
+  public struct BlockBuilder<T> where T: TimeSetable, T: SequenceItem {
+    public static func buildBlock(_ components: T...) -> ContiguousArray<T> {
+      var start = TimeInterval.zero
+      let new = components.map { item in
+        item.setTime(time: start)
+        start += item.timeInterval
+        return item
+      }
+      return ContiguousArray(new)
     }
+  }
+
+  private let queue = DispatchQueue(
+    label: "SequenceMain"
+    , qos: .default,
+    attributes: .concurrent,
+    autoreleaseFrequency: .inherit,
+    target: nil
+  )
+
+  private var blocks: ContiguousArray<T>
+
+  private var count: Int {
+    blocks.count
+  }
+
+  private var passedTime: TimeInterval = .zero
+
+  public init(@BlockBuilder<T> content: () -> ContiguousArray<T>) {
+    self.blocks = content()
+  }
+
+  public func run() {
+    blocks.forEach { block in
+      queue.asyncAfter(deadline: .now() + block.timeInterval) {
+        block.workItem()
+      }
+    }
+  }
 }
